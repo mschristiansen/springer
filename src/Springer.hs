ca4430ceebd8ce5c84b781a67e12e0e122144d20
Mikkel Christiansen <mikkel@rheosystems.com>
2021-11-08 19:39:06 +0100

    Simple commandloop

--- src/Springer.hs
+++ src/Springer.hs
@@ -1,6 +1,6 @@
 module Springer (main) where
 
-import Data.List
+import Control.Monad (unless)
 import Springer.Parse (parseWith)
 import Springer.UciParser (GuiCommand (..), guiCommandParser)
 import System.IO
@@ -9,29 +9,34 @@ main :: IO ()
 main = do
   let logFile = "/tmp/springer.log"
   hSetBuffering stdout NoBuffering
-  let go = do
-        s <- getLine
-        appendFile logFile ("< " <> s <> "\n")
-        let resp = handleCommand s
-        mapM_ (\s -> appendFile logFile ("> " <> s <> "\n")) resp
-        mapM_ putStrLn resp
-        go
-  go
+  appendFile logFile "Springer: starting...\n"
+  commandLoop logFile
+  appendFile logFile "Springer: goodbye...\n"
 
-handleCommand :: String -> [String]
-handleCommand s = respond $ parseWith guiCommandParser s
+commandLoop :: FilePath -> IO ()
+commandLoop logFile = do
+  s <- getLine
+  appendFile logFile ("gui    : " <> s <> "\n")
+  let cmd = parseWith guiCommandParser s
+  reply logFile $
+    case cmd of
+      Uci -> ["id name Springer", "id author Mikkel", "uciok"]
+      Debug Nothing -> ["debug switch"]
+      Debug (Just True) -> ["debug on"]
+      Debug (Just False) -> ["debug off"]
+      SetOption _ _ -> ["setoption"]
+      Register -> []
+      IsReady -> ["readyok"]
+      UciNewGame -> []
+      PositionStart _ -> []
+      Position _fen -> []
+      Go _ -> []
+      Stop -> []
+      PonderHit -> []
+      Quit -> ["info goodbye"]
+  unless (cmd == Quit) $ commandLoop logFile
 
-respond :: GuiCommand -> [String]
-respond command =
-  case command of
-    Uci -> ["id name Springer", "id author Mikkel", "uciok"]
-    Debug Nothing -> ["debug switch"]
-    Debug (Just True) -> ["debug on"]
-    Debug (Just False) -> ["debug off"]
-    IsReady -> ["readyok"]
-    UciNewGame -> []
-    PositionStart -> []
-    Go _ -> []
-    Stop -> []
-    PonderHit -> []
-    Quit -> []
+reply :: FilePath -> [String] -> IO ()
+reply logFile cmds = do
+  mapM_ (\l -> appendFile logFile ("engine : " <> l <> "\n")) cmds
+  mapM_ putStrLn cmds
